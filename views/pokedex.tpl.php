7dc602a343b4693a5b5ccda6de3eb47bcf73ac1a
PierreMorin4590 <pierremorin4590@hotmail.fr>
2023-12-20 16:22:42 +0100

    Version OK en faisant F5

--- views/pokedex.tpl.php
+++ views/pokedex.tpl.php
@@ -1,16 +1,16 @@
 <main class="main">
-        <div class="main__div">
-            <div class="main__div--pokemon">
-                // On dynamise l'affichage de l'image du Pokémon
-                <img src="<?= $pokemonData->sprites->front_default; ?>" width="180" alt="">
-            </div>
-            <div class="main__div--number">
-                // On dynamise l'affichage du numéro du Pokémon
-                <?='#'.$pokemonDataSpecies->id ?>
-            </div>
-            <div class="main__div--name">
-                // On dynamise l'affichage du nom français du Pokémon
-                <?=$pokemonDataSpecies->names[4]->name; ?>
-            </div>
+    <div class="main__div">
+        <div class="main__div--pokemon">
+            <!-- On dynamise l'affichage de l'image du Pokémon -->
+            <img src="<?= $pokemonData->sprites->front_default; ?>" width="180" alt="">
         </div>
-    </main>
+        <div class="main__div--number">
+            <!-- On dynamise l'affichage du numéro du Pokémon -->
+            <?= '#' . $pokemonDataSpecies->id ?>
+        </div>
+        <div class="main__div--name">
+            <!-- On dynamise l'affichage du nom français du Pokémon -->
+            <?= $pokemonDataSpecies->names[4]->name; ?>
+        </div>
+    </div>
+</main>
