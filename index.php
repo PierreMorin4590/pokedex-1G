12d8985b26c168d023138e9c50a55591c5937fdd
PierreMorin4590 <pierremorin4590@hotmail.fr>
2023-12-20 16:16:00 +0100

    Ajout de commentaire et des méthodes de récupération des data

--- index.php
+++ index.php
@@ -3,19 +3,18 @@
 $pokemon = "http://pokeapi.co/api/v2/pokemon/";
 $pokemonSpecies = "http://pokeapi.co/api/v2/pokemon-species/";
 
-$id = 5;
+$id = rand(1, 151);
 
-$data = file_get_contents($pokemon.$id.'/');
-$dataSpecies = file_get_contents($pokemonSpecies.$id.'/');
+$data = file_get_contents($pokemon.$id);
+$dataSpecies = file_get_contents($pokemonSpecies.$id);
 
 $pokemonData = json_decode($data);
 $pokemonDataSpecies =json_decode($dataSpecies);
 
-$pokemonData->sprites->front_default;
-
-//print_r($pokemon->names);
-echo $pokemonDataSpecies->names[4]->name;
-echo $pokemonDataSpecies->id;
+// Afficher l'image de face du Pokémon : $pokemonData->sprites->front_default;
+// Afficher le nom français du Pokémon : echo $pokemonDataSpecies->names[4]->name;
+// Afficher l'id du Pokémon : echo $pokemonDataSpecies->id;
+// Afficher le type du Pokémon : print_r($pokemonData->types[0]->type->name);
 
 require_once './views/header.tpl.php';
 require_once './views/pokedex.tpl.php';
